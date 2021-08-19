d3c46f9de0f358b6c1c75dc0c2a39948b4c0fda4
John Palgut <jwsonic@gmail.com>
2021-08-19 20:52:53 +0000

    Finish virtual connection tests

--- ui/test/ui/printer/connection/virtual_test.exs
+++ ui/test/ui/printer/connection/virtual_test.exs
@@ -7,20 +7,27 @@ defmodule Ui.Printer.Connection.VirtualTest do
   alias Ui.Printer.Connection
   alias Ui.Printer.Connection.Virtual
 
-  setup do
+  setup context do
     connection = Virtual.new()
 
-    {:ok, connection} = Connection.connect(connection)
+    connection =
+    case context[:no_connect] do
+      true -> connection
+      _ ->
+        {:ok, connection} = Connection.connect(connection)
 
-    on_exit(fn -> Connection.disconnect(connection) end)
+        on_exit(fn -> Connection.disconnect(connection) end)
+
+        connection
+    end
 
     {:ok, %{connection: connection}}
   end
 
   describe "Virtual.connect/1" do
-    test "it spawns a virtual printer port" do
-      connection = Virtual.new()
 
+    @tag :no_connect
+    test "it spawns a virtual printer port", %{connection: connection} do
       assert connection.port == nil
       assert connection.reference == nil
 
@@ -53,6 +60,7 @@ defmodule Ui.Printer.Connection.VirtualTest do
       Port.info(connection.port)
     end
 
+    @tag :no_connect
     test "it does nothing if there's no port open", %{connection: connection} do
       assert connection.port == nil
       assert connection.reference == nil
@@ -62,18 +70,30 @@ defmodule Ui.Printer.Connection.VirtualTest do
   end
 
   describe "Virtual.send/2" do
-    test "it sends the message to the given port/pid", %{connection: connection} do
-      {:ok, connection} = Connection.connect(connection)
+    test "it sends the message to the given port/pid", %{connection: %{port: port} = connection} do
+      :erlang.trace(port, true,  [:receive])
+
+      me = self()
 
       Connection.send(connection, "G10\n")
 
-      assert_receive {_pid, {:connection_data, "ok"}}, 1_000
+      assert_receive {:trace, ^port, :receive, {^me, {:command, "G10\n"}}}, 1_000
     end
   end
 
   describe "Virtual.update/2" do
-    test "it forwards port messages as connection messages"
-    test "it handles port close events"
-    test "it ignores other messages"
+    test "it forwards port messages as connection messages", %{connection: %{port: port} = connection} do
+      {:ok, _connection} = Connection.update(connection, {port, {:data, "test"}})
+
+      assert_receive {:connection_data, "test"}
+    end
+
+    test "it handles port close events", %{connection: %{port: port, reference: reference} = connection}  do
+      assert {:error, ~s(Port closed: "It went boom")} == Connection.update(connection, {:DOWN, reference, :port, port, "It went boom"})
+    end
+
+    test "it ignores other messages", %{connection: connection} do
+      assert {:ok, connection} == Connection.update(connection, :yo)
+    end
   end
 end
