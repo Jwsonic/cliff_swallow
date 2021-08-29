f376f9c4906b5bde1c2b25097895d721c5543a05
John Palgut <jwsonic@gmail.com>
2021-08-28 21:27:06 -0400

    Refactor out gcode

--- ui/lib/ui/printer.ex
+++ ui/lib/ui/printer.ex
@@ -2,6 +2,9 @@ defmodule Ui.Printer do
   @moduledoc """
   API for the `Ui.Printer` domain.
   """
+  use Norms
+
+  alias Ui.Printer.Gcode
   alias Ui.Printer.Server, as: PrinterServer
 
   def connect(connection) do
@@ -10,5 +13,16 @@ defmodule Ui.Printer do
 
   def send(command) do
     GenServer.call(PrinterServer, {:send, command})
+  end
+
+  def move(args) do
+    args
+    |> Gcode.move()
+    |> send()
+  end
+
+  @contract heat(temp :: int_or_float()) :: simple_result()
+  def heat(temp) do
+    send("M109 S#{temp}\n")
   end
 end
